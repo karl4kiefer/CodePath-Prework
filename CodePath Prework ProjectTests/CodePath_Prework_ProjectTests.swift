3b72995408b46e2525c33c2c32db8bef826ebe35
Karl Kiefer IV <karl4kiefer@outlook.com>
2025-04-13 12:35:02 -0500

    Added Prework Files to Repo

---
+++ CodePath Prework ProjectTests/CodePath_Prework_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodePath_Prework_ProjectTests.swift
+//  CodePath Prework ProjectTests
+//
+//  Created by Karl Kiefer IV on 4/12/25.
+//
+
+import Testing
+@testable import CodePath_Prework_Project
+
+struct CodePath_Prework_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
